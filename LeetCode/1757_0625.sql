62fc0e56923a352cf6654a1c2004188c999f6862
mini-u <sungminryu127@gmail.com>
2025-06-25 17:17:41 +0900

    [LeetCode-1757] Recyclable and Low Fat Products - SQL

--- LeetCode/1757_0625.sql
+++ LeetCode/1757_0625.sql
@@ -1,8 +1,3 @@
-'''
-Write a solution to find the ids of products that are both low fat and recyclable.
-Return the result table in any order.
-The result format is in the following example.
-'''
-
+#1757
 SELECT product_id FROM Products
 WHERE (low_fats = 'Y') and (recyclable = 'Y')
