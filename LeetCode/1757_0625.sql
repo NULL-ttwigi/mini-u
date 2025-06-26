d265ce53873567c2f4be853523fb79cf73809877
mini-u <sungminryu127@gmail.com>
2025-06-26 10:54:56 +0900

    [LC-1757] Recyclable and Low Fat Products

--- LeetCode/1757_0625.sql
+++ LeetCode/1757_0625.sql
@@ -1,3 +1,6 @@
-#1757
+# 문제: [1757] Recyclable and Low Fat Products
+# 난이도: Easy
+# 링크: https://leetcode.com/problems/recyclable-and-low-fat-products?envType=study-plan-v2&envId=top-sql-50
+
 SELECT product_id FROM Products
 WHERE (low_fats = 'Y') and (recyclable = 'Y')
